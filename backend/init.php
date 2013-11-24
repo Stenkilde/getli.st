65aeb952dde77f1208664a1079c3e5d7899fc42e
rediche <weblrmail@gmail.com>
2013-11-24 18:17:02 +0100

    Bugfix

--- backend/init.php
+++ backend/init.php
@@ -2,9 +2,9 @@
 
 //Start User sessions
 session_start();
-require '/connect/config.php';
-require '/classes/Users.php';
-require '/classes/General.php';
+require 'connect/config.php';
+require 'classes/Users.php';
+require 'classes/General.php';
 
 $users 		= new Users($db);
 $general 	= new General();
