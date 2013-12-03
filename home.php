00a2503966954d333ae08b9d00defd82fdcaa61a
Oliver Stenkilde <oliver@stenkilde.com>
2013-12-03 21:49:00 +0100

    init for static html

--- home.php
+++ home.php
@@ -8,10 +8,20 @@ $email 		= $user['email'];
 ?>
 
 
-<h1>Hello <?php echo $email . '!'; ?></h1>
+<h1>Goodmorning<!-- (Skal skifte alt efter hvad tid på dagen det er) --> <?php echo $email . '!'; ?></h1>
 <a href="logout.php">Logout</a>
 
-
+<section class="row">
+	<div class="listShower column large-12">
+		<ul class="activeLists">
+			<li><a href="#">Morgenamds indkøb</a></li>
+			<li><a href="#">Indkøb til Fest</a></li>
+			<li><a href="#">Liste Nummer 3</a></li>
+			<li><a href="#">4</a></li>
+			<li><a href="#">5</a></li>
+		</ul>
+	</div>
+</section>
 
 <?php
 //Requires footer.php to function
